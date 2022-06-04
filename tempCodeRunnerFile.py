86e23de9a1b55f90d6146f2c1c0579a107f930af
ObbyTrusty <70574883+ObbyTrusty@users.noreply.github.com>
2022-06-04 11:14:04 -0700

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-guilds = ["6111fcb48ea8c95240436c57", "604a765e8ea8c962f2bb3b7a",
-                  "607a0d7c8ea8c9c0ff983976", "608d91e98ea8c9925cdb91b7",
-                "60a16b088ea8c9bb7f6d9052", "60b923478ea8c9a3aefbf3dd", "6125800e8ea8c92e1833e851",
-                  "570940fb0cf2d37483e106b3"]
